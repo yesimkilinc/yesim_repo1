5d70a47edeccb89d9309d80690e62b2a52cfaf28
Git_Yesim <TCYKILINC@turkcell.entp.tgc>
2024-01-30 14:08:29 +0300

    6.sorunun commit işlemidir.

---
+++ question_6.sql
@@ -0,0 +1,5 @@
+6)Write a brief explanation about DevOps. (no longer than 1 page).
+
+DevOps: Geliştirmeyle operasyonu bir araya getirir.  Şirketlerin bilgi teknolojileri(IT) hizmetleriyle beraber yazılım ürünlerinin de daha hızlı ve daha sık teslim edilebilir (continuous delivery) olmasına yardımcı olmaktadır.
+DevOps'un Avantajları
+Hız, Hızlı Teslim, Güvenilirlik, Geliştirilmiş İş Birliği
