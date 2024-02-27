9250ba6c8091458af7b57283723c652bc1f7222d
yesimkilinc <yesim.kilinc@turkcell.com.tr>
2024-02-27 10:46:24 +0300

    Update question_6.sql

--- question_6.sql
+++ question_6.sql
@@ -1,5 +1,7 @@
 6)Write a brief explanation about DevOps. (no longer than 1 page).
 
 DevOps: Geliştirmeyle operasyonu bir araya getirir.  Şirketlerin bilgi teknolojileri(IT) hizmetleriyle beraber yazılım ürünlerinin de daha hızlı ve daha sık teslim edilebilir (continuous delivery) olmasına yardımcı olmaktadır.
-DevOps'un Avantajları
-Hız, Hızlı Teslim, Güvenilirlik, Geliştirilmiş İş Birliği
+
+DevOps'un Avantajları; Hız, Hızlı Teslim, Güvenilirlik, Geliştirilmiş İş Birliği
+ 
+Devops'ların kullandığı populer toolar; Jenkins, Kubernetes, Github vs.
